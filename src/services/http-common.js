89c8572e0e1f6b87b753a311724be38b650c43e0
Devanand Galande <devanandgalande98@gmail.com>
2024-01-01 13:21:36 +0530

    added deployment config

--- src/services/http-common.js
+++ src/services/http-common.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 export default axios.create({
-  baseURL: "https://fair-rose-lemur-gown.cyclic.app/api",
-  // baseURL: "http://localhost:8080/api",
+  // baseURL: "https://fair-rose-lemur-gown.cyclic.app/api",
+  baseURL: process.env.SERVER_URL || "http://localhost:8080/api",
   headers: {
     "Content-type": "application/json"
   }
